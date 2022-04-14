453f177d10cc9ad7fc02a4d64c99e60971a89844
Kevinnkf <101806653+Kevinnkf@users.noreply.github.com>
2022-04-14 13:54:25 +0700

    Add files via upload

---
+++ projectoop/ProjectOOP.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectoop;
+
+import FrameKursi.Studio;
+
+/**
+ *
+ * @author USER
+ */
+public class ProjectOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Studio studio = new Studio();
+        studio.setLocationRelativeTo(null);
+        studio.setVisible(true);
+    }
+    
+}
